db32ebbda6725b7af58323d6746ac108a79b2a68
smylinwolf <fabianohaz@gmail.com>
2022-07-08 18:56:47 -0700

    A program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,23 +4,22 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == "__main__":
-
     length = len(argv)
-if length - 1 != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-if length == 4:
-    operator = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
-    if operator == "+":
-        print(f"{a} + {b} = {a + b}")
-    elif operator == "-":
-        print(f"{a} - {b} = {a - b}")
-    elif operator == "*":
-        print(f"{a} * {b} = {a * b}")
-    elif operator == "/":
-        print(f"{a} / {b} = {a / b}")
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    if length == 4:
+        operator = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+        if operator == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif operator == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif operator == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif operator == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
