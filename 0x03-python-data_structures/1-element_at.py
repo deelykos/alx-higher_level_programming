2977af29e08b6dd92a506a88a0b2b5c66fa8694f
Ohazulume C. Fabian <fabianohaz@gmail.com>
2022-07-16 11:26:11 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     for i in my_list:
-        if idx < 0 and idx > range(my_list):
+        if idx < 0 and idx > len(my_list) - 1:
             return (None)
         else:
             return (my_list[idx])
