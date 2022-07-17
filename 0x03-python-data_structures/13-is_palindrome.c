c14054e5fec5c5fb58333a8f360d605e33c7d1b2
smylinwolf <fabianohaz@gmail.com>
2022-07-17 11:35:08 -0700

    Technical interview preparation:ls

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,47 +1,72 @@
 #include "lists.h"
 
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: head of singly linked list
- * Return: 1 if palindrome else 0
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *temp = *head;
-	int head_val, tail_val;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	/* empty linked list condition */
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	/* get head value for comparison */
-	head_val = (*head)->n;
-	while (current)
+	tmp = *head;
+	while (tmp)
 	{
-		/* look ahead to see if next node from current is the tail */
-		if ((current->next)->next == NULL)
-		{
-			/* assign val of tail for comparison */
-			tail_val = (current->next)->n;
-			/* update tail to current */
-			current->next = NULL;
-
-			if (head_val != tail_val)
-				return (0);
-
-			/* update head (N/B: temp is serving as the head) */
-			temp = temp->next;
-			/* is a palindrome if head is NULL */
-			if (!temp)
-				break;
-
-			/* update head_val and current for traversing */
-			head_val = temp->n;
-			current = temp;
-		}
-		else
-			current = current->next;
+		size++;
+		tmp = tmp->next;
 	}
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
 	return (1);
 }
