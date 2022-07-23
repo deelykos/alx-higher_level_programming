8d33a0413935a06d6a09004d93c05567131824ca
smylinwolf <fabianohaz@gmail.com>
2022-07-23 11:45:35 -0700

    converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    my_dictionary = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    my_dictionary = {'M': 1000, 'D': 500, 'C': 100,
+                     'L': 50, 'X': 10, 'V': 5, 'I': 1}
     if roman_string is None or type(roman_string) != str:
         return 0
 
@@ -11,8 +12,8 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         curr = my_dictionary[roman_string[i]]
         if curr > prev:
-            total += curr -2 * prev
+            total += curr - 2 * prev
         else:
             total += curr
         prev = curr
-    return total    
+    return total
