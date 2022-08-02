a149ac110c17812383b1b0441970f829d017603b
Ohazulume C. Fabian <fabianohaz@gmail.com>
2022-08-02 21:36:52 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-""" An empty class Square that defines a square """
+""" An empty class Square that defines a square. """
 
 
 class Square:
+    """ Defines a Square. """
     pass
