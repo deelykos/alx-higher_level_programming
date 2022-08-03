403daf66abefebdc525ff6c09b296b8ce3d205aa
smylinwolf <fabianohaz@gmail.com>
2022-08-02 17:29:59 -0700

    Property and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Defines a class square """
+
+
+class Square:
+    """ This represents a square """
+    def __init__(self, size=0):
+        """Instantiate a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Get the current size of square. """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ set the current size of square """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Method that returns current Square area """
+        return self.__size ** 2
