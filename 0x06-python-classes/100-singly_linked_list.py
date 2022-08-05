b175fd904498a9f195ea576150381d18fe9fc69b
smylinwolf <fabianohaz@gmail.com>
2022-08-05 06:45:56 -0700

    Advanced 1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -15,29 +15,27 @@ class Node:
         self.data = data
         self.next_node = next_node
 
-        @property
-        def data(self):
-            """ Get the data of the node. """
-            return (self.__data)
+    @property
+    def data(self):
+        """Get/set the data of the Node."""
+        return (self.__data)
 
-        @data.setter
-        def data(self, value):
-            """ Set the value of the data """
-            if not isinstance(value, int):
-                raise TypeError("data must be an integer")
-            self.__data = value
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
 
-        @property
-        def next_node(self):
-            """ Set the value of the new node """
-            return (self.__next_node)
+    @property
+    def next_node(self):
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
 
-        @next_node.setter
-        def next_node(self, value):
-            """ Set the value of the new node """
-            if not isinstance(value, Node) and value is not None:
-                raise TypeError("next_node must be a Node object")
-            self.__next_node = value
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
 
 class SinglyLinkedList:
