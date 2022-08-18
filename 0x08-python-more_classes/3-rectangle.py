49d87a673ca3d0def76a1e559d5e85b9e75afbe3
smylinwolf <fabianohaz@gmail.com>
2022-08-18 07:24:48 -0700

    Task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" This module creates a Rectangle """
+
+
+class Rectangle:
+    """Defines a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initiation of Rectangle
+        Args:
+            width (int): Width must be an integer
+            height (int): height must be an integer
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the current width of Rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the current height of Rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Return area of Rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """Return perimeter of a Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2 * self.__height) + (2 * self.__width)
+
+    def __str__(self):
+        """Return the printable representation of a Rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return ("")
+
+        rect_str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rect_str += "#"
+            if j != self.__height - 1:
+                rect_str += "\n"
+        return rect_str
