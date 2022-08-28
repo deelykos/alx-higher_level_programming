c5e6e0ea16ae4639091229f09738bbb232549a52
smylinwolf <fabianohaz@gmail.com>
2022-08-27 19:26:07 -0700

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
+    # if in task 9 I used str.(self.__class__.__name__) instead of the class
+    #name, I won't have added this method. Because my super() would cover
+    #for it.
