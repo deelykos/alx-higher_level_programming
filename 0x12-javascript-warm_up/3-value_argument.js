d9f53f1aee53482ad46353fd066e850396575331
smylinwolf <fabianohaz@gmail.com>
2022-10-07 06:31:20 -0700

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv;
-if (arg !== process.argv[2]) {
+const arg = process.argv[2];
+if (arg === undefined)
+{
   console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+  console.log(arg);
 }
